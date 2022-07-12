2171662ba54d68ed8ea13a1549342a90e4a4474a
raviteja <ravitejamerugu@gmail.com>
2022-07-12 12:24:41 +0530

    working k8s cluster

---
+++ db-init.js
@@ -0,0 +1,11 @@
+db.createUser({
+      user: "user",
+      pwd: "secretPassword",
+      roles: [ { role: "dbOwner", db: "realestate" } ]
+    });
+
+    db.users.insert({
+      name: "user"
+    })
+
+
